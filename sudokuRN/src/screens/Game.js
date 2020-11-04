41cfe4d9ab12856855dd511755d03fc7abe01b19
riankarya <riankaryamarpaung@gmail.com>
2020-11-04 17:56:16 +0700

    waktu abis udah di handle

--- sudokuRN/src/screens/Game.js
+++ sudokuRN/src/screens/Game.js
@@ -3,9 +3,7 @@ import { View, Text, StyleSheet, Button, TextInput, KeyboardAvoidingView } from 
 import { useNavigation } from '@react-navigation/native'
 import { fetchBoard, changeBoard, addLeaderBoard } from '../stores/action'
 import { useSelector, useDispatch } from 'react-redux'
-import valid from '../helpers/cekInput'
 import CountDown from 'react-native-countdown-component'
-import { ADD_LEADERBOARD } from '../stores/actionTypes'
 
 export default function Game(props) {
   const { name, difficulty } = props.route.params
@@ -22,11 +20,10 @@ export default function Game(props) {
     if (isWin) {
       dispatch(addLeaderBoard({name, timer, isFinish: true, difficulty}))
     } else {
-      dispatch(addLeaderBoard({name, timer: 1800, isFinish: false, difficulty}))
+      dispatch(addLeaderBoard({name, timer: 1200, isFinish: false, difficulty}))
     }
   }
   async function onPressToFinish() {
-    // console.log(board, 'ONPRESS FINISH LUHUR')
     const encodeBoard = (board) => board.reduce((result, row, i) => result + `%5B${encodeURIComponent(row)}%5D${i === board.length -1 ? '' : '%2C'}`, '')
     const encodeParams = (params) => 
       Object.keys(params)
@@ -34,35 +31,24 @@ export default function Game(props) {
       .join('&')
     const newBoard = []
     const tempBoard = JSON.parse(JSON.stringify(board))
-    // console.log(board, JSON.stringify(board), JSON.parse(JSON.stringify(board)), 'asup ti stringify');
     while(tempBoard.length) newBoard.push(tempBoard.splice(0, 9))
-    // while(board.length) newBoard.push(board.splice(0, 9))
     console.log(newBoard, newBoard.length, 'asup ti tempboard');
     const data = {board: newBoard}
-    fetch('https://sugoku.herokuapp.com/solve', {
+    fetch('https://sugoku.herokuapp.com/validate', {
       method: 'POST',
       body: encodeParams(data),
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
     })
       .then(response => response.json())
       .then(response => {
-        console.log(response.status, 'asup ti then response');
-        finishState(true)
-        navigation.replace('Finish')
         if (response.status == 'solved') {
-          // console.log(board, 'ONPRESS FINISH HANDAP')
-          // renderBoard()
           finishState(true)
           navigation.replace('Finish')
         } else {
           alert('Wrong Solution')
-          console.log('wrong solution');
         }
       })
       .catch(console.warn)
-      // .catch(err => {
-      //   console.log(err)
-      // })
   }
   useEffect(() => {
     let interval = setInterval(() => {
@@ -89,26 +75,29 @@ export default function Game(props) {
   }
   return (
     <View style={styles.container}>
+      <Text>Halo { name }</Text>
       <View style={{flex: 1, flexDirection: 'row', flexWrap: 'wrap', width: 290}}>
       {renderBoard()}
       <KeyboardAvoidingView keyboardVerticalOffset={-170}>
         <View style={{marginTop: 20, marginLeft: '35%', justifyContent: 'center'}}>
           <Button title='Go to Home' onPress={ onPressToHome }></Button>
-          <Button title='Go to Finish' onPress={ onPressToFinish }></Button>
+          <Button title='Finish' onPress={ onPressToFinish }></Button>
         </View>
       </KeyboardAvoidingView>
-      <Text>Time Remaining</Text>
+      <Text style={{width: '93%', textAlign: 'center'}}>Time Remaining</Text>
       <CountDown
-        until={60 * 10 + 30}
+        until={60 * 20}
         size={20}
-        onFinish={() => alert('Finished')}
+        onFinish={() => {
+          alert("Time's Up")
+          finishState(false)
+          navigation.replace('Finish')
+        }}
         digitStyle={{backgroundColor: '#FFF'}}
         digitTxtStyle={{color: '#1CC625'}}
         timeToShow={['M', 'S']}
+        style={{width: '93%', textAlign: 'center'}}
       />
-      <Text>Ini Game {timer}</Text>
-      <Text>ahidaw {difficulty}</Text>
-      <Text>Halo { name }</Text>
       </View>
     </View>
   )
